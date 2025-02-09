978411a8ef3f56e546d96d4cf11e3ccc19f241df
MrDeveloper07 <prathameshgodage@gmail.com>
2025-02-09 19:21:05 +0530

    udpate

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
-import basic from '../assets/basic.png'
-import lite from '../assets/lite.png'
-import pro from '../assets/pro.png'
-import Advanced from '../assets/advaned.png'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import basic from "../assets/basic.png";
+import lite from "../assets/lite.png";
+import pro from "../assets/pro.png";
+import Advanced from "../assets/advaned.png";
 import { FaCrown } from "react-icons/fa";
+import axios from "axios";
 export default function Homepage() {
-  const sub="pro";
+  const [subscription, setSubscription] = useState("");
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get("http://localhost:5000/api/auth/me", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        console.log(response.data);
+        setSubscription(response.data.subscription);
+        // Set the profile image URL
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const sub = subscription;
 
   const manage = () => {
     let imagetype; // Declare variables outside of the condition
@@ -22,60 +45,105 @@ export default function Homepage() {
     } else if (sub === "pro") {
       imagetype = pro;
       typename = "Pro";
-    }
-    else{
-      imagetype = Advanced
+    } else {
+      imagetype = Advanced;
       typename = "Advanced";
-
     }
 
     return { imagetype, typename }; // Return both variables
   };
-const data=manage();
-console.log(data.typename)
+  const data = manage();
+  console.log(data.typename);
+  // fetchUser();
 
   return (
-    <div className='flex  h-full  items-center justify-between w-full  flex-col bg-slate-800'>
-        <div className="h-24  w-full p-8 flex  items-center"><img src={logo  } alt="" className='h-16'/></div>
-        <div className="h-[400px] flex items-center flex-col">
-        <Link to="/image" className="h-14 m-3 w-72 border-solid border-2 border-white  bg-orange-500 items-center justify-center flex rounded-xl">
-          <span className="text-white font-semibold font-lato">Image Generation</span>
+    <div className="flex  h-full  items-center justify-between w-full  flex-col bg-slate-800">
+      <div className="h-24  w-full p-8 flex  items-center">
+        <img src={logo} alt="" className="h-16" />
+      </div>
+      <div className="h-[400px] flex items-center flex-col">
+        <Link
+          to="/image"
+          className="h-14 m-3 w-72 border-solid border-2 border-white  bg-orange-500 items-center justify-center flex rounded-xl"
+        >
+          <span className="text-white font-semibold font-lato">
+            Image Generation
+          </span>
         </Link>
-     <Link to="/videoGeneration" className="h-14 m-3 w-72  border-solid border-2 border-white    bg-orange-500 items-center justify-center font-lato flex rounded-xl "><span className='text-white font-semibold'>Video Generation</span></Link>
-     <Link to="/pptGeneration" className="h-14 m-3 w-72 bg-orange-500 border-solid border-2 border-white  items-center justify-center font-lato flex rounded-xl "><span className='text-white font-semibold'>PPT Generation</span></Link>
-     <Link to="/musicGeneration" className="h-14 m-3 w-72 bg-orange-500 border-solid border-2 border-white  items-center justify-center font-lato flex rounded-xl "><span className='text-white font-semibold'>Music Generation</span></Link>
-     <div className="relative">
-     <Link to="/Intereview Preparation" className={`h-14  m-3 w-72 z-10 bg-red-600  ${sub!="advanced"? "blur-sm":""} border-solid border-2 border-white  items-center justify-center font-lato flex rounded-xl `}><span className='text-white font-semibold'>Interview Preparation</span>
-   
-   </Link>
-   {sub!="advanced"  ?    <div className="absolute top-3 left-3 rounded-xl h-14 w-72 border-2  bg-gray-500  bg-opacity-60 flex items-center justify-center gap-4"><FaCrown size={28} color='#ffb83d'/> <span className='text-lg font-bold  text-white'> Advanced Membership</span></div>: <></>}
-
-     </div>
-    
-
+        <Link
+          to="/videoGeneration"
+          className="h-14 m-3 w-72  border-solid border-2 border-white    bg-orange-500 items-center justify-center font-lato flex rounded-xl "
+        >
+          <span className="text-white font-semibold">Video Generation</span>
+        </Link>
+        <Link
+          to="/pptGeneration"
+          className="h-14 m-3 w-72 bg-orange-500 border-solid border-2 border-white  items-center justify-center font-lato flex rounded-xl "
+        >
+          <span className="text-white font-semibold">PPT Generation</span>
+        </Link>
+        <Link
+          to="/musicGeneration"
+          className="h-14 m-3 w-72 bg-orange-500 border-solid border-2 border-white  items-center justify-center font-lato flex rounded-xl "
+        >
+          <span className="text-white font-semibold">Music Generation</span>
+        </Link>
+        <div className="relative">
+          <Link
+            to="/Intereview Preparation"
+            className={`h-14  m-3 w-72 z-10 bg-red-600  ${
+              sub != "advanced" ? "blur-sm" : ""
+            } border-solid border-2 border-white  items-center justify-center font-lato flex rounded-xl `}
+          >
+            <span className="text-white font-semibold">
+              Interview Preparation
+            </span>
+          </Link>
+          {sub != "advanced" ? (
+            <div className="absolute top-3 left-3 rounded-xl h-14 w-72 border-2  bg-gray-500  bg-opacity-60 flex items-center justify-center gap-4">
+              <FaCrown size={28} color="#ffb83d" />{" "}
+              <span className="text-lg font-bold  text-white">
+                {" "}
+                Advanced Membership
+              </span>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
+      </div>
 
-        <div className="h-44 flex p-4 px-6 justify-center items-center  w-full ">
-           <div className="h-36 w-72 mx-4   flex border-white rounded-xl justify-center items-center overflow-hidden relative">
-
-            <div className="h-full w-full bg-gradient-to-tr rounded-xl from-indigo-500 via-purple-500 via-blue-500 via-yellow-700 to-pink-500"></div>
-            <div className={`absolute h-[135px] w-[280px]  top-1 ${sub=="basic"? "bg-gray-600" : sub=="lite"? "bg-green-600" : sub=="pro" ? "bg-violet-700":""}  rounded-xl px-2 flex`}>
-            <div className="w-48   flex justify-center items-center "><img src={data.imagetype} alt="" className='h-24 w-full'/></div>
+      <div className="h-44 flex p-4 px-6 justify-center items-center  w-full ">
+        <div className="h-36 w-72 mx-4   flex border-white rounded-xl justify-center items-center overflow-hidden relative">
+          <div className="h-full w-full bg-gradient-to-tr rounded-xl from-indigo-500 via-purple-500 via-blue-500 via-yellow-700 to-pink-500"></div>
+          <div
+            className={`absolute h-[135px] w-[280px]  top-1 ${
+              sub == "basic"
+                ? "bg-gray-600"
+                : sub == "lite"
+                ? "bg-green-600"
+                : sub == "pro"
+                ? "bg-violet-700"
+                : ""
+            }  rounded-xl px-2 flex`}
+          >
+            <div className="w-48   flex justify-center items-center ">
+              <img src={data.imagetype} alt="" className="h-24 w-full" />
+            </div>
             <div className="flex w-full justify-between py-6 flex-col gap-3 items-center">
-<div className="text-white text-3xl font-premium capitalize">{data.typename}</div>
-<Link to="/manageMembership" className="h-12 text-center border-2 px-4 w-36 text-white font-base justify-center rounded-xl flex items-center font-imprima ">Manage Membership</Link>
+              <div className="text-white text-3xl font-premium capitalize">
+                {data.typename}
+              </div>
+              <Link
+                to="/manageMembership"
+                className="h-12 text-center border-2 px-4 w-36 text-white font-base justify-center rounded-xl flex items-center font-imprima "
+              >
+                Manage Membership
+              </Link>
             </div>
-
-            </div>
-         
-          
-
-           </div>
+          </div>
         </div>
-
-   
-
-
+      </div>
     </div>
-  )
+  );
 }
--- src/components/SubcriptionPages/Subscription.jsx
+++ src/components/SubcriptionPages/Subscription.jsx
@@ -1,81 +1,178 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 import { FaShieldVirus } from "react-icons/fa6";
 import { RiCustomerService2Line } from "react-icons/ri";
 import { RiVisaFill } from "react-icons/ri";
 import { CgPaypal } from "react-icons/cg";
 import { MdOutlinePayments } from "react-icons/md";
+import axios from "axios";
 export default function Subscription() {
-  return (
-    <div className='h-full w-full bg-slate-750 flex flex-col items-center pt-12'>
+  const [subscription, setSubscription] = useState("");
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get("http://localhost:5000/api/auth/me", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        console.log(response.data);
+        setSubscription(response.data.subscription);
+        // Set the profile image URL
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const sub = subscription;
+  console.log(sub);
+  return (
+    <div className="h-full w-full bg-slate-750 flex flex-col items-center pt-12">
       <div className=" text-2xl font-semibold text-white">Our Pricing</div>
-      <div className="text-ms text-gray-300 mt-2 font-semibold">Team Active offers you the most friendly packages to start your design career</div>
+      <div className="text-ms text-gray-300 mt-2 font-semibold">
+        Team Active offers you the most friendly packages to start your design
+        career
+      </div>
       <div className="h-[360px] w-full px-8 gap-2 mt-10 flex justify-between">
-        <div className="border-4 border-green-400  from-slate-100 bg-gradient-to-b w-64 my-4 rounded-[35px] flex flex-col items-center">
+        <div
+          className={`${
+            sub == "basic"
+              ? " border-4 border-green-400 "
+              : "border-2 border-white"
+          }   from-slate-100 bg-gradient-to-b w-64 my-4 rounded-[35px] flex flex-col items-center`}
+        >
           <div className="mt-6 text-black font-bold text-2xl">Free</div>
-          <div className="w-full text-center flex justify-center text-sm font-semibold mt-4 ">A suitable Way to start design career for student</div>
+          <div className="w-full text-center flex justify-center text-sm font-semibold mt-4 ">
+            A suitable Way to start design career for student
+          </div>
           <div className="h-36 flex items-center text-xl font-semibold">
-           <div className="flex items-">
-           0.00 
-           <div className="h-10  justify-none  text-[10x]"> Rs</div>
-           </div>
+            <div className="flex items-">
+              0.00
+              <div className="h-10  justify-none  text-[10x]"> Rs</div>
+            </div>
           </div>
-          <div className="text-[10px] font-medium">Hover to see more features</div>
-        </div>
-       <div className="relative">
-       <div className="border-2 border-white h-[328px]  from-blue-600 bg-gradient-to-t to-slate-200 w-64 my-4 rounded-[35px] flex flex-col items-center">
-          <div className="mt-6 text-black font-bold text-2xl">Lite</div>
-          <div className="w-full text-center flex justify-center text-sm font-semibold mt-4 ">A suitable Way to start design career for student</div>
-          <div className="h-36 flex items-center text-xl font-semibold">
-           <div className="flex items-">
-           50.00 
-           <div className="h-10  justify-none  text-[10x]"> Rs</div>
-           </div>
-          </div>
-          <div className="text-[10px] font-medium">Hover to see more features</div>
-        </div>
-        <div className="absolute bottom-0 h-8  w-full">
-          <div className="w-full justify-center h-full flex  items-center">
-            <div className="w-36 rounded-3xl h-full border-2 border-white bg-gray-300 text-center cursor-pointer flex items-center justify-center font-semibold">Subscribe </div>
+          <div className="text-[10px] font-medium">
+            Hover to see more features
           </div>
         </div>
-       </div>
-       <div className="relative">
-       <div className="border-2 border-white h-[328px]  from-violet-600 bg-gradient-to-t to-slate-200 w-64 my-4 rounded-[35px] flex flex-col items-center">
-          <div className="mt-6 text-black font-bold text-2xl">Pro</div>
-          <div className="w-full text-center flex justify-center text-sm font-semibold mt-4 ">A suitable Way to start design career for student</div>
-          <div className="h-36 flex items-center text-xl font-semibold">
-           <div className="flex items-">
-           50.00 
-           <div className="h-10  justify-none  text-[10x]"> Rs</div>
-           </div>
+        <div className="relative">
+          <div
+            className={`${
+              sub == "lite"
+                ? " border-4 border-green-400 "
+                : "border-2 border-white"
+            } h-[328px]  from-blue-600 bg-gradient-to-t to-slate-200 w-64 my-4 rounded-[35px] flex flex-col items-center`}
+          >
+            <div className="mt-6 text-black font-bold text-2xl">Lite</div>
+            <div className="w-full text-center flex justify-center text-sm font-semibold mt-4 ">
+              A suitable Way to start design career for student
+            </div>
+            <div className="h-36 flex items-center text-xl font-semibold">
+              <div className="flex items-">
+                50.00
+                <div className="h-10  justify-none  text-[10x]"> Rs</div>
+              </div>
+            </div>
+            <div className="text-[10px] font-medium">
+              Hover to see more features
+            </div>
           </div>
-          <div className="text-[10px] font-medium">Hover to see more features</div>
-        </div>
-        <div className="absolute bottom-0 h-8  w-full">
-          <div className="w-full justify-center h-full flex  items-center">
-            <div className="w-36 rounded-3xl h-full border-2 border-white bg-gray-300 text-center cursor-pointer flex items-center justify-center font-semibold">Subscribe </div>
+          <div className="absolute bottom-0 h-8  w-full">
+            <div className="w-full justify-center h-full flex  items-center">
+              <div
+                className={` ${
+                  sub == "lite" ? "bg-red-500 text-white" : " bg-gray-300 "
+                } w-36 rounded-3xl h-full border-2 border-whitetext-center cursor-pointer flex items-center justify-center font-semibold`}
+              >
+                {sub === "lite" ? (
+                  <span>Subscribed</span>
+                ) : (
+                  <span>Subscribe</span>
+                )}
+              </div>
+            </div>
           </div>
         </div>
-       </div>
-       <div className="relative">
-       <div className="border-2 border-white h-[328px]  from-red-600 bg-gradient-to-t to-slate-200 w-64 my-4 rounded-[35px] flex flex-col items-center">
-          <div className="mt-6 text-black font-bold text-2xl">Advanced</div>
-          <div className="w-full text-center flex justify-center text-sm font-semibold mt-4 ">A suitable Way to start design career for student</div>
-          <div className="h-36 flex items-center text-xl font-semibold">
-           <div className="flex items-">
-           250.00 
-           <div className="h-10  justify-none  text-[10x]"> Rs</div>
-           </div>
+        <div className="relative">
+          <div
+            className={`${
+              sub == "pro"
+                ? " border-4 border-green-400 "
+                : "border-2 border-white"
+            } h-[328px]  from-blue-600 bg-gradient-to-t to-slate-200 w-64 my-4 rounded-[35px] flex flex-col items-center`}
+          >
+            <div className="mt-6 text-black font-bold text-2xl">Pro</div>
+            <div className="w-full text-center flex justify-center text-sm font-semibold mt-4 ">
+              A suitable Way to start design career for student
+            </div>
+            <div className="h-36 flex items-center text-xl font-semibold">
+              <div className="flex items-">
+                150.00
+                <div className="h-10  justify-none  text-[10x]"> Rs</div>
+              </div>
+            </div>
+            <div className="text-[10px] font-medium">
+              Hover to see more features
+            </div>
           </div>
-          <div className="text-[10px] font-medium">Hover to see more features</div>
-        </div>
-        <div className="absolute bottom-0 h-8  w-full">
-          <div className="w-full justify-center h-full flex  items-center">
-            <div className="w-36 rounded-3xl h-full border-2 border-white bg-gray-300 cursor-pointer text-center flex items-center justify-center font-semibold">Subscribe </div>
+          <div className="absolute bottom-0 h-8  w-full">
+            <div className="w-full justify-center h-full flex  items-center">
+              <div
+                className={` ${
+                  sub == "pro" ? "bg-red-500 text-white" : " bg-gray-300 "
+                } w-36 rounded-3xl h-full border-2 border-whitetext-center cursor-pointer flex items-center justify-center font-semibold`}
+              >
+                {sub === "pro" ? (
+                  <span>Subscribed</span>
+                ) : (
+                  <span>Subscribe</span>
+                )}
+              </div>
+            </div>
           </div>
         </div>
-       </div>
+        <div className="relative">
+          <div
+            className={`${
+              sub == "advanced"
+                ? " border-4 border-green-400 "
+                : "border-2 border-white"
+            } h-[328px]  from-blue-600 bg-gradient-to-t to-slate-200 w-64 my-4 rounded-[35px] flex flex-col items-center`}
+          >
+            <div className="mt-6 text-black font-bold text-2xl">Advanced</div>
+            <div className="w-full text-center flex justify-center text-sm font-semibold mt-4 ">
+              A suitable Way to start design career for student
+            </div>
+            <div className="h-36 flex items-center text-xl font-semibold">
+              <div className="flex items-">
+                250.00
+                <div className="h-10  justify-none  text-[10x]"> Rs</div>
+              </div>
+            </div>
+            <div className="text-[10px] font-medium">
+              Hover to see more features
+            </div>
+          </div>
+          <div className="absolute bottom-0 h-8  w-full">
+            <div className="w-full justify-center h-full flex  items-center">
+              <div
+                className={` ${
+                  sub == "advanced" ? "bg-red-500 text-white" : " bg-gray-300 "
+                } w-36 rounded-3xl h-full border-2 border-whitetext-center cursor-pointer flex items-center justify-center font-semibold`}
+              >
+                {sub === "advanced" ? (
+                  <span>Subscribed</span>
+                ) : (
+                  <span>Subscribe</span>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="w-full h-48 justify-around flex items-center">
         <div className="w-72 h-24 flex flex-col justify-center  items-center px-4">
@@ -83,22 +180,25 @@ export default function Subscription() {
             ACCEPTED PAYMENT METHODS
           </div>
           <div className="w-full gap-2 ml-14 flex">
-          <RiVisaFill size={28} color='white'/>
-          <CgPaypal  size={28} color='white'/>
-          <MdOutlinePayments size={28} color='white' />
+            <RiVisaFill size={28} color="white" />
+            <CgPaypal size={28} color="white" />
+            <MdOutlinePayments size={28} color="white" />
           </div>
-
         </div>
         <div className="w-72 h-24  flex items-center px-4">
-        <RiCustomerService2Line  color='white' size={48}/>
-        <div className=" ml-4 font-semibold text-white text-[12px]">It you are not satisfied with our services then contact with our Team Leader</div>
+          <RiCustomerService2Line color="white" size={48} />
+          <div className=" ml-4 font-semibold text-white text-[12px]">
+            It you are not satisfied with our services then contact with our
+            Team Leader
+          </div>
         </div>
         <div className="w-72 h-24 flex items-center px-4">
-          <FaShieldVirus color='white' size={38}/>
-          <div className=" ml-4 font-semibold text-white text-[12px]">Your Information is Protected By Our Team</div>
+          <FaShieldVirus color="white" size={38} />
+          <div className=" ml-4 font-semibold text-white text-[12px]">
+            Your Information is Protected By Our Team
+          </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
